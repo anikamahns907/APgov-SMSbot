9d5abc51eeb3da5d70e0386f77016683e4bbfb05
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2020-05-09 03:03:21 -0400

    completed

--- SMSbot.py
+++ SMSbot.py
@@ -1,38 +1,97 @@
+import time
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
 from twilio.rest import Client
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 from datetime import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 
 app = Flask(__name__)
-
-#This function logs into twilio and then sends
-#This function only runs every 30 mins
-currentMessage = ""
-def job_function():
+currentMessage = 'test'
+def sendText(message):
     print('sending message')
-    # f = open("question.txt", "r")
-    # for line in f:
-    #     line = line.split('\n')[0]
-    #     print(line)
-    #     if line == '':
-    #         continue
-    #     currentMessage = line
-    #     f.write(line)
-    #     break
     account_sid = 'AC65c3a80eeee91f09f5b5dc086594be30' #get SID number
-    auth_token = '153680b34b75a54d2012d9f69d9c9317' #get your auth_token
+    auth_token = '0fa5bf72afe7b8796385c52fb7ff61cb' #get your auth_token
     client = Client(account_sid, auth_token) 
+    body = message
     message = client.messages.create(
-    body= currentMessage,
+    body= body,
     from_='[+][1][8329900263]', 
     to='[+][1][9078021615]')
     print(message.sid)
 
+    
+def job_function():
+    print('sending message')
+    f = open("question.txt", "r", encoding="utf-8")
+    for line in f:
+        isPresent = False
+        print("kaka")
+        line = line.split('\n')[0]
+        print(line)
+        if line == '':
+            print('tata')
+            continue
+        f2 = open("usedQs.txt", "r", encoding="utf-8")
+        print('caca')
 
+        for line2 in f2: 
+            print("jaja")
+            line2 = line2.split('\n')[0]
+            if line2 == line:
+                isPresent = True
+
+        if (isPresent):
+            continue
+        else:
+            f2.close()
+            f2 = open("usedQs.txt", "a", encoding="utf-8")
+            f2.write(line + "\n")
+            global currentMessage
+            currentMessage = line 
+            print("yay")
+            sendText(line)
+            f.close()
+            f2.close()
+            return True
+
+
+
+
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(job_function,'interval',seconds = 5)
+sched.start()
+
+
+def createToken():
+    print('sunset')
+        # required for all twilio access tokens
+    account_sid = 'AC65c3a80eeee91f09f5b5dc086594be30'
+    api_key = 'SK0c50fe6ce0d409929bad25e7b92e0927'
+    api_secret = 'pJ0cBU90MsYZ3CgHWmPmJmStUH5he9bR'
+
+    # required for Chat grants
+    identity = 'anika'
+
+    # Create access token with credentials
+    token = AccessToken(account_sid, api_key, api_secret, identity=identity)
+
+    # Create a Video grant and add to token
+    video_grant = VideoGrant(room='DailyStandup')
+    token.add_grant(video_grant)
+
+    # Return token info as JSON
+    print(token.to_jwt())
+    return(token.to_jwt())
+
+
+#This function logs into twilio and then sends
+#This function only runs every 30 mins
 
 
 #This function handles responses
@@ -51,9 +110,4 @@ def bot():
         msg.body('Close... try again! Read the message over!')
     return str(resp)
 if __name__ == "__main__":
-    app.run(debug=True)
-
-    sched = BlockingScheduler()
-    # Schedule job_function to be called every two hours
-    sched.add_job(job_function, 'interval', minutes = 1)
-    sched.start()
+    app.run(use_reloader=False)
--- test.py
+++ test.py
@@ -1,20 +1,134 @@
+import json
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
+from twilio.rest import Client
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-f = open("question.txt", "r")
-wrong = True
-for line in f:
-    line = line.split('\n')[0]
-    print(line)
+
+def createToken():
+        # required for all twilio access tokens
+    account_sid = 'AC65c3a80eeee91f09f5b5dc086594be30'
+    api_key = 'SK0c50fe6ce0d409929bad25e7b92e0927'
+    api_secret = 'pJ0cBU90MsYZ3CgHWmPmJmStUH5he9bR'
+
+    # required for Chat grants
+    identity = 'anika'
+
+    # Create access token with credentials
+    token = AccessToken(account_sid, api_key, api_secret, identity=identity)
+
+    # Create a Video grant and add to token
+    video_grant = VideoGrant(room='DailyStandup')
+    token.add_grant(video_grant)
+
+    # Return token info as JSON
+    print(type(token.to_jwt()))
+
+    realToken = str(token.to_jwt())
+    realToken = realToken.split('.')[-1]
+    # print(realToken)
+    return(realToken)
 
 
-    while(wrong):
-        response = input("Copy this text: ")
-        print(response, line)
-        if (str(response) == str(line)):
-            print('success')
-            break
-        else:
-            print('fail')
+
+
+def job_function():
+    print('sending message')
+    account_sid = 'AC65c3a80eeee91f09f5b5dc086594be30' #get SID number
+    auth_token = createToken()
+    client = Client(account_sid, auth_token) 
+    message = client.messages.create(
+    body='Time for your daily training POOP!',
+    from_='[+][1][8329900263]', 
+    to='[+][1][9078021615]'
+    )
+    print(message.sid)
+
+job_function()
+
+
+# f = open("question.txt", "r")
+# wrong = True
+# for line in f:
+#     line = line.split('\n')[0]
+#     print(line)
+
+
+#     while(wrong):
+#         response = input("Copy this text: ")
+#         print(response, line)
+#         if (str(response) == str(line)):
+#             print('success')
+#             break
+#         else:
+#             print('fail')
            
+
+
+# from twilio.rest import Client
+# from flask import Flask, request
+# import requests
+# from twilio.twiml.messaging_response import MessagingResponse
+# from datetime import datetime
+# from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+
+# app = Flask(__name__)
+
+# #This function logs into twilio and then sends
+# #This function only runs every 30 mins
+# currentMessage = ""
+# def job_function():
+#     print('sending message')
+#     # f = open("question.txt", "r")
+#     # for line in f:
+#     #     line = line.split('\n')[0]
+#     #     print(line)
+#     #     if line == '':
+#     #         continue
+#     #     currentMessage = line
+#     #     f.write(line)
+#     #     break
+#     account_sid = 'AC65c3a80eeee91f09f5b5dc086594be30' #get SID number
+#     auth_token = '0fa5bf72afe7b8796385c52fb7ff61cb' #get your auth_token
+#     client = Client(account_sid, auth_token) 
+#     message = client.messages.create(
+#     body= currentMessage,
+#     from_='[+][1][8329900263]', 
+#     to='[+][1][9078021615]')
+#     print(message.sid)
+
+
+
+
+# #This function handles responses
+# #This function runs only when theres a response (response in the POST setting to webhook /bot)
+# @app.route('/bot', methods=['POST'])
+# def bot():
+#     incoming_msg = request.values.get('Body', '').lower()
+#     resp = MessagingResponse()
+#     msg = resp.message()
+#     responded = False
+#     if currentMessage in incoming_msg:
+#         # return a quote
+#         msg.body('success genius!')
+#         responded = True
+#     if not responded:
+#         msg.body('Close... try again! Read the message over!')
+#     return str(resp)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+#     sched = BlockingScheduler()
+#     # Schedule job_function to be called every two hours
+#     sched.add_job(job_function, 'interval', minutes = 1)
+#     sched.start()
+
 
 
 
